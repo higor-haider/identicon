58b6c7141f2a0b24ed4fe0df24abf380ac817f4c
higor-haider <higor.haider@sumup.com>
2022-06-16 17:10:59 -0300

    Adds mirror_row and build_grid functions

--- lib/identicon.ex
+++ lib/identicon.ex
@@ -1,8 +1,26 @@
 defmodule Identicon do
-  def main (input) do
+  def main(input) do
     input
     |> hash_input
     |> pick_color
+    |> build_grid
+  end
+
+  def build_grid(%Identicon.Image{hex: hex} = image) do
+    grid =
+      hex
+      |> Enum.chunk(3)
+      |> Enum.map(&mirror_row/1)
+      |> List.flatten
+      |> Enum.with_index
+
+    %Identicon.Image{image | grid: grid}
+  end
+
+  def mirror_row(row) do
+    [first, second | _tail] = row
+
+    row ++ [second, first]
   end
 
   def pick_color(%Identicon.Image{hex: [r, g, b | _tail]} = image) do
@@ -10,8 +28,9 @@ defmodule Identicon do
   end
 
   def hash_input(input) do
-    hex = :crypto.hash(:md5, input)
-    |> :binary.bin_to_list
+    hex =
+      :crypto.hash(:md5, input)
+      |> :binary.bin_to_list()
 
     %Identicon.Image{hex: hex}
   end
